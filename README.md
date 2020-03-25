882153a45664016f84cbdb93035417ec1613107b
root <mohsenzarein1379@gmail.com>
2020-03-25 17:10:51 +0430

    specific directory for each subject and some improvement

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Image-Scraper
+
+search for anything you want and download some related images ...
--- image-scraper.py
+++ image-scraper.py
@@ -2,32 +2,38 @@ from bs4 import BeautifulSoup
 import requests
 from io import BytesIO
 from PIL import Image
+import os
 
-search = input("Search for:")
-params = {"q":search}
 
-req = requests.get("https://www.bing.com/images/search",params=params)
-print(req.status_code)
+def SearchForImage():
 
-soup = BeautifulSoup(req.text,"html.parser")
-link = soup.findAll("a",{"class":"thumb"})
+    search = input("Search for:")
+    params = {"q":search}
+    directory_name = search.replace(' ','_')
 
-path = "/home/mohsen/VSCode/ImageScraper/scraped_image/"
+    if not os.path.isdir(directory_name):
+        os.mkdir(directory_name)
 
-for item in link:
-    try:
-        imgObj = requests.get(item.attrs["href"])
-        print(imgObj.status_code)
-        print("Getting ",item.attrs["href"],"...")
-        title = item.attrs["href"].split('/')[-1]
+    req = requests.get("https://www.bing.com/images/search",params=params)
+    #print(req.status_code)
 
+    soup = BeautifulSoup(req.text,"html.parser")
+    link = soup.findAll("a",{"class":"thumb"})
+
+    for item in link:
         try:
-            image = Image.open(BytesIO(imgObj.content))
-            image.save(path + title , image.format)
+            imgObj = requests.get(item.attrs["href"])
+            print("Getting ",item.attrs["href"],"...")
+            title = item.attrs["href"].split('/')[-1]
+            try:
+                image = Image.open(BytesIO(imgObj.content))
+                image.save("./" + directory_name + "/" + title, image.format)
+            except:
+                print("could not save image")
         except:
-            print("could not save image")
-    except:
-        print("connection failed")
+            print("Could not reach a website ...")
+    SearchForImage()
 
+SearchForImage()
 
 
